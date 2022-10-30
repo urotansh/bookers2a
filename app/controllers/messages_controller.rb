6c0b5fd5d044e9226fa31509c94d53bd0c8f1d27
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-31 02:39:59 +0900

    [Add] メッセージ投稿機能の作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,4 +7,19 @@ class MessagesController < ApplicationController
     @messages = Message.all
     
   end
+  
+  def create
+    message = Message.new(message_params)
+    message.user_id = current_user.id
+    message.save
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:body)
+  end
+  
+  
 end
